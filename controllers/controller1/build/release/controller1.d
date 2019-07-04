b06f421e44cd532086cc5e7f51dd7a9d9a6ba724
Emiliano Zarzoza Estrada <emilianozarzozaestrada@Emilianos-MacBook-Pro.local>
2019-07-04 13:44:40 -0500

    Robot has been finished but with some doubts about its functioning

---
+++ controllers/controller1/build/release/controller1.d
@@ -0,0 +1,8 @@
+build/release/controller1.o: controller1.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/keyboard.h \
+  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h \
+  /Applications/Webots.app/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/include/controller/c/webots/position_sensor.h
--- controllers/controller1/controller1.c
+++ controllers/controller1/controller1.c
@@ -11,11 +11,10 @@
 #define OBSTACLE_DISTANCE 25.0
 #define PI 3.14159
 
-
 int main(int argc, char **argv) {
 
   wb_robot_init();
-  wb_keyboard_enable();
+  wb_keyboard_enable(TIME_STEP);
 
   int key_pressed;
   double PosSensor1 = 0;
@@ -26,83 +25,160 @@ int main(int argc, char **argv) {
   double ComparingValue = 0;
   double distSensor1_value = 0;
   double distSensor2_value = 0;
+  int w = 0;
+  int g = 0;
 
   //distance sensor components
   WbDeviceTag dist_sensor[2];
-  WbDeviceTag dist_sensor[1] = wb_robot_get_device("dist_sensor1");
-  WbDeviceTag dist_sensor[2] = wb_robot_get_device("dist_sensor2");
+  dist_sensor[0] = wb_robot_get_device("dist_left_sensor1");
+  dist_sensor[1] = wb_robot_get_device("dist_right_sensor2");
 
+  wb_distance_sensor_enable(dist_sensor[0], TIME_STEP);
   wb_distance_sensor_enable(dist_sensor[1], TIME_STEP);
-  wb_distance_sensor_enable(dist_sensor[2], TIME_STEP);
 
   //Motor components
   WbDeviceTag wheels[3];
-  wheels[1] = wb_robot_get_device("right_wheel")
-  wheels[2] = wb_robot_get_device("left_wheel")
-  wheels[3] = wb_robot_get_device("back_wheel")
+  wheels[0] = wb_robot_get_device("right_wheel");
+  wheels[1] = wb_robot_get_device("left_wheel");
+  wheels[2] = wb_robot_get_device("back_wheel");
 
-  wb_motor_set_position (wheel[1], INFINITY);
-  wb_motor_set_position (wheel[2], INFINITY);
-  wb_motor_set_position (wheel[3], INFINITY);
+  wb_motor_set_position(wheels[0], INFINITY);
+  wb_motor_set_position(wheels[1], INFINITY);
+  wb_motor_set_position(wheels[2], INFINITY);
 
   //encoder components
   WbDeviceTag encoder[3];
-  encoder[1] = wb_robot_get_device("pos_sensor1")
-  encoder[2] = wb_robot_get_device("pos_sensor2")
-  encoder[3] = wb_robot_get_device("pos_sensor3")
+  encoder[0] = wb_robot_get_device("pos_sensor1");
+  encoder[1] = wb_robot_get_device("pos_sensor2");
+  encoder[2] = wb_robot_get_device("pos_sensor3");
 
+  wb_position_sensor_enable(encoder[0], TIME_STEP);
   wb_position_sensor_enable(encoder[1], TIME_STEP);
   wb_position_sensor_enable(encoder[2], TIME_STEP);
-  wb_position_sensor_enable(encoder[3], TIME_STEP);
 
-  while (wb_robot_step(TIME_STEP) != -1) {
+void ManualMode(){
 
-    PosSensor1 = wb_position_sensor_get_value(encoder[1]);
-    PosSensor2 = wb_position_sensor_get_value(encoder[2]);
-    PosSensor3 = wb_position_sensor_get_value(encoder[3]);
-    printf("encoder_1: %lf\n", Encoder[1]);
-    printf("encoder_2: %lf\n", encoder[2]);
-    printf("encoder_3: %lf\n", encoder[3]);
-
-    key_pressed = wb_keyboard_get_key();
-
-    printf("Comparing angle value %lf\n", ComparingValue);
+    PosSensor1 = wb_position_sensor_get_value(encoder[0]);
+    PosSensor2 = wb_position_sensor_get_value(encoder[1]);
+    PosSensor3 = wb_position_sensor_get_value(encoder[2]);
+    printf("encoder_1: %lf\n", encoder[0]);
+    printf("encoder_2: %lf\n", encoder[1]);
+    printf("encoder_3: %lf\n", encoder[2]);
 
     distSensor1_value = wb_distance_sensor_get_value(dist_sensor[1]);
     distSensor2_value = wb_distance_sensor_get_value(dist_sensor[2]);
     printf("Distance sensor 1: %lf\n", distSensor1_value);
     printf("Distance sensor 2: %lf\n", distSensor2_value);
 
+    printf("Comparing angle value %lf\n", ComparingValue);
 
     if (key_pressed == WB_KEYBOARD_DOWN) {
-      wb_motor_set_velocity(wheels[1], 6.36);
-      wb_motor_set_velocity(wheel[2], -6.36);
-      wb_motor_set_velocity(wheel[3], 0);
+      wb_motor_set_velocity(wheels[0], 6.36);
+      wb_motor_set_velocity(wheels[1], -6.36);
+      wb_motor_set_velocity(wheels[2], 0);
     } else if (key_pressed == WB_KEYBOARD_UP) {
-      wb_motor_set_velocity(wheel[1], -6.36);
-      wb_motor_set_velocity(wheel[2], 6.36);
-      wb_motor_set_velocity(wheel[3], 0);
+      wb_motor_set_velocity(wheels[0], -6.36);
+      wb_motor_set_velocity(wheels[1], 6.36);
+      wb_motor_set_velocity(wheels[2], 0);
     }  else if (key_pressed == WB_KEYBOARD_LEFT) {
-      wb_motor_set_velocity(wheel[1], 0);
-      wb_motor_set_velocity(wheel[2], -6.36);
-      wb_motor_set_velocity(wheel[3], 6.36);
+      wb_motor_set_velocity(wheels[0], 0);
+      wb_motor_set_velocity(wheels[1], -6.36);
+      wb_motor_set_velocity(wheels[2], 6.36);
     } else if (key_pressed == WB_KEYBOARD_RIGHT) {
-      wb_motor_set_velocity(wheel[1], 6.36);
-      wb_motor_set_velocity(wheel[2], 0);
-      wb_motor_set_velocity(wheel[3], -6.36);
+      wb_motor_set_velocity(wheels[0], 6.36);
+      wb_motor_set_velocity(wheels[1], 0);
+      wb_motor_set_velocity(wheels[2], -6.36);
     } else if (key_pressed == 'S' ) {
-          ComparingValue = PosSensor1 + 0.785398;
-          left = 1;
-         }
+      ComparingValue = PosSensor1 + 0.785398;
+        left = 1;
+    } else if (left == 1) {
+        if (PosSensor1 <= ComparingValue) {
+          wb_motor_set_velocity(wheels[0], 6.36);
+          wb_motor_set_velocity(wheels[1], 6.36);
+          wb_motor_set_velocity(wheels[2], 6.36);
+        } else {
+          wb_motor_set_velocity(wheels[0], 0);
+          wb_motor_set_velocity(wheels[1], 0);
+          wb_motor_set_velocity(wheels[2], 0);
+          left = 0;
+        }
+      } else if (key_pressed == 'A') {
+        ComparingValue = PosSensor1 - 0.785398;
+        right = 1;
+      } else if (right == 1) {
+        if (PosSensor1 >= ComparingValue) {
+          wb_motor_set_velocity(wheels[0], -6.36);
+          wb_motor_set_velocity(wheels[1], -6.36);
+          wb_motor_set_velocity(wheels[2], -6.36);
+        } else {
+          wb_motor_set_velocity(wheels[0], 0);
+          wb_motor_set_velocity(wheels[1], 0);
+          wb_motor_set_velocity(wheels[2], 0);
+          right = 0;
+        }
+      } else {
+        wb_motor_set_velocity(wheels[0], 0);
+        wb_motor_set_velocity(wheels[1], 0);
+        wb_motor_set_velocity(wheels[2], 0);
+      }
+  }
 
-         
-         }
+  void AutonomousMode(){
+
+    PosSensor1 = wb_position_sensor_get_value(encoder[0]);
+    PosSensor2 = wb_position_sensor_get_value(encoder[1]);
+    PosSensor3 = wb_position_sensor_get_value(encoder[2]);
+    printf("encoder_1: %lf\n", encoder[0]);
+    printf("encoder_2: %lf\n", encoder[1]);
+    printf("encoder_3: %lf\n", encoder[2]);
+
+    distSensor1_value = wb_distance_sensor_get_value(dist_sensor[0]);
+    distSensor2_value = wb_distance_sensor_get_value(dist_sensor[1]);
+    printf("Distance sensor 1: %lf\n", distSensor1_value);
+    printf("Distance sensor 2: %lf\n", distSensor2_value);
+
+    printf("Comparing angle value %lf\n", ComparingValue);
+
+    wb_motor_set_velocity(wheels[0], -6.66);
+    wb_motor_set_velocity(wheels[1], 6.66);
+    wb_motor_set_velocity(wheels[2], 0);
+
+    if(distSensor1_value < distSensor2_value && dist_left_sensor1 < 200) {
+      wb_motor_set_velocity(wheels[0], 6.66);
+      wb_motor_set_velocity(wheels[1], 0);
+      wb_motor_set_velocity(wheels[2], -6.66);
+    } else if (distSensor1_value > distSensor2_value && distSensor2_value < 200) {
+      wb_motor_set_velocity(wheels[0], -6.66);
+      wb_motor_set_velocity(wheels[1], 0);
+      wb_motor_set_velocity(wheels[2], 6.66);
+    }
+  }
+
+  wb_motor_set_velocity(wheels[0], 0);
+  wb_motor_set_velocity(wheels[1], 0);
+  wb_motor_set_velocity(wheels[2], 0);
+
+
+  while (wb_robot_step(TIME_STEP) != -1) {
+    key_pressed = wb_keyboard_get_key();
+
+    if(key_pressed == 'W') {
+      w = 1;
+      g = 0;
+    } else if (key_pressed == 'G') {
+      g = 1;
+      w = 0;
+    }
+    printf("w = %i     \n", w);
+    printf("g = %i     \n", g);
+
+    if(w == 1)
+    ManualMode();
+    if(g == 1)
+    AutonomousMode();
 
   };
 
-  /* Enter your cleanup code here */
-
-  /* This is necessary to cleanup webots resources */
   wb_robot_cleanup();
 
   return 0;
